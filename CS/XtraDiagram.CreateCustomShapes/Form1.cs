f4fee684422ddc1a322e82a20c6502d8bd11bb60
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/XtraDiagram.CreateCustomShapes/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.Diagram.Core;
+using DevExpress.Diagram.Core.Shapes;
+using System.Windows;
+using System.IO;
+using System.Reflection;
+
+namespace XtraDiagram.CreateCustomShapes {
+    public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm {
+        public Form1() {
+            InitializeComponent();
+            RegisterCustomShapes();
+            this.diagramControl1.SelectedStencils = new StencilCollection(new string[] { "CustomShapes" });
+        }
+        protected override void OnLoad(EventArgs e) {
+            base.OnLoad(e);
+            this.diagramControl1.InitializeRibbon(this.ribbonControl1);
+        }
+
+        void RegisterCustomShapes() {
+            using(var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("XtraDiagram.CreateCustomShapes.CustomShapes.xml")) {
+                var stencil = DiagramStencil.Create("CustomShapes", "Custom Shapes", stream, shapeName => shapeName);
+                DiagramToolboxRegistrator.RegisterStencil(stencil);
+            }
+        }
+    }
+    
+}
---
+++ VB/XtraDiagram.CreateCustomShapes/Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.Diagram.Core
+Imports DevExpress.Diagram.Core.Shapes
+Imports System.Windows
+Imports System.IO
+Imports System.Reflection
+
+Namespace XtraDiagram.CreateCustomShapes
+    Partial Public Class Form1
+        Inherits DevExpress.XtraBars.Ribbon.RibbonForm
+
+        Public Sub New()
+            InitializeComponent()
+            RegisterCustomShapes()
+            Me.diagramControl1.SelectedStencils = New StencilCollection(New String() {"CustomShapes"})
+        End Sub
+        Protected Overrides Sub OnLoad(ByVal e As EventArgs)
+            MyBase.OnLoad(e)
+            Me.diagramControl1.InitializeRibbon(Me.ribbonControl1)
+        End Sub
+
+        Private Sub RegisterCustomShapes()
+            Using stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("CustomShapes.xml")
+                Dim stencil = DiagramStencil.Create("CustomShapes", "Custom Shapes", stream, Function(shapeName) shapeName)
+                DiagramToolboxRegistrator.RegisterStencil(stencil)
+            End Using
+        End Sub
+    End Class
+
+End Namespace
